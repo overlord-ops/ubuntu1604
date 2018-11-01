d9d29264bd24a76d20165a5c052feb3a52d1db90
CobraKenji <froggyofapex@gmail.com>
2018-11-01 17:54:06 +0000

    update

--- bash/node-admin-configure.sh
+++ bash/node-admin-configure.sh
@@ -15,6 +15,10 @@ YELLOW="\033[0;33m"
 CYAN="\033[0;36m"
 NC='\e[0m'
 
+get_latest_release() {
+  curl --silent "$1" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
+}
+
 PKGS='git vim nano wget htop gzip ansible autotools-dev automake omake cmake libssl-dev libtool curl tree rsync software-properties-common pkg-config libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev libdb4.8-dev libdb4.8++-dev'
 
 clear
@@ -29,7 +33,7 @@ echo ""
 echo ""
 
 PS3="Please choose a task number (press enter to view menu) : "
-options=("initial setup" "add sudo user" "reset user password" "install fail2ban" "configure ipv6" "task6" "task7" "task8" "Quit")
+options=("initial setup" "add sudo user" "reset user password" "install fail2ban" "configure ipv6" "task6" "task7" "update node" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -38,11 +42,11 @@ do
 	 echo "${GREEN}this will update then isntall following packages via apt-get: ${NC}"
 	 echo -e "${YELLOW} $PKGS ${NC}";
 	 echo ""
-	 #sudo apt-get update
-	 #sudo apt-get upgrade -y
-	 #for pkg in $PKGS; do sudo apt-get install -y $pkg; done
+	 sudo apt-get update
+	 sudo apt-get upgrade -y
+	 for pkg in $PKGS; do sudo apt-get install -y $pkg; done
 	 echo -e "${GREEN} almost done... ${NC}"
-	 #sudo apt-get update
+	 sudo apt-get update
 	 curl https://raw.githubusercontent.com/CobraKenji/thawed/master/ansible/keyzon/keyzon-fullplay.yml -o keyzon-fullplay.yml
 	 ansible-playbook keyzon-fullplay.yml
 	 RESULT=$?
@@ -61,7 +65,7 @@ do
 	 echo -e "${GREEN} Grant sudo to a user. Creates new sudo user if not present. ${NC}"
 	 echo -e "${YELLOW}!!! This will grant passwordless sudo access !!! ${NC}"
 	 read -e -p "Enter sudo user name : " newSudoer
-         echo -e "${YELLOW} Veryfing passwordless sudo access for $newSudeor ... ${NC}";
+         echo -e "${YELLOW} Veryfing passwordless sudo access for $newSudoer ... ${NC}";
          echo ""
 	 echo ""
 	 curl https://raw.githubusercontent.com/CobraKenji/ubuntu1604/master/ansible/create-sudoer.yml -o create-sudoer.yml
@@ -90,9 +94,39 @@ do
         echo "";
             ;;
          "install fail2ban")
-	read -e -p "Prompt " var1
-        echo "$var1".conf
-        echo "";
+	read -e -p "Install Fail2ban? [y/n]" yesno
+
+          case "$yesno" in
+            y|Y )
+		echo -e "${CYAN}Select an install profile: basic or strict?${NC}"
+		read -e -p " : " f2bType
+                case "$f2bType" in
+                *basic*)
+                        echo -e configuring "${GREEN}basic fail2ban${NC}" setup
+                        echo ""
+			sudo apt-get install fail2ban -y
+                        echo -e "${YELLOW} Fail2ban installed ${NC}";
+                        echo -e "${CYAN}you can edit the configuration at ${YELLOW}/etc/fail2ban/jail.local${CYAN} - Please be careful if you decide to edit this.${NC}"
+                        ;;
+                *strict*)
+                        echo -e "${GREEN}NEED TO FINISH${NC}"
+                        echo ""
+                        echo -e "${CYAN}to install Fail2ban with a ${GREEN}HEAVY${NC}${CYAN} ban hammer${NC}"
+			sudo apt-get install fail2ban -y
+			sudo sed -i 's/bantime  = 600/bantime  = 10000/1' jail.conf
+			sudo sed -i 's/findtime  = 600/findtime  = 10000 /1' jail.conf
+			echo -e "${CYAN}Fail2ban installed and configured. More than 5 failed login attempts${NC}"
+			echo -e "${CYAN}in 2.75 hours ${YELLOW}WILL BLACKLIST ${CYAN} your IP address for 2.75 hours.${NC}"
+			echo -e "${YELLOW}Please do not guess your SSH password.${NC}"
+                        ;;
+                *)
+                        echo -e "${YELLOW}$f2bType is not a valid option, idiot. Check your spelling.${NC}";
+                        echo "";
+                        esac
+                        ;;
+            n|N ) ;;
+            * ) echo "invalid";;
+            esac
             ;;
          "configure ipv6")
 	read -e -p "Question? : " var2
@@ -113,11 +147,24 @@ do
         echo EMPTY TASK SLOT 
         echo "";
             ;;
-	 "task8")
-        echo you are currently managing as user: "$(whoami)".
-	echo EMPTY TASK SLOT
-	read -e -p "Some var prompt " someVar
-        echo "$someVar";
+	 "update node")
+        echo You are currently managing as user: "$(whoami)".
+	echo -e "${YELLOW}You will need the URL to the updated version.${NC}"
+	echo -e "${YELLOW}This will wget the tar, un-archive, then install the new version${NC}"
+	echo -e "${YELLOW}No version checking is performed. Use with caution.${NC}"
+	read -e -p "Enter URL to new wallet version : " newVersion
+        echo "$newVersion";
+
+	get_latest_release 'https://github.com/BlockchainFor/ESportBettingCoin/releases'
+	curl -s https://api.github.com/repos/BlockchainFor/ESportBettingCoin/releases/latest \
+	| grep "browser_download_url" \
+	| cut -d : -f 2,3 \
+	| tr -d \" \
+	| wget -qi -
+
+# my epic one-liner is maybe viable
+curl -s https://api.github.com/repos/BlockchainFor/ESportBettingCoin/releases/latest | grep "browser_download_url.*tar.gz" | cut -d : -f 2,3 | sed '1q' | tr -d \" | xargs
+
             ;;
 
 
